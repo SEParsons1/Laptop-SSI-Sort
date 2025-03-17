17a82ee565fa7aa40b943b89bfd37b3a952f89ff
SEParsons1 <sparsonsbc@outlook.com>
2025-03-17 12:07:59 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,88 @@
+let inputTimer;
+
+function handleInput() {
+  clearTimeout(inputTimer);
+  const input = document.getElementById('myInput');
+  let cleanedInput = input.value.toUpperCase().replace(/[^A-Z0-9]/g, '');
+  if (cleanedInput.length >= 3) {
+    const lastThree = cleanedInput.slice(-3);
+    inputTimer = setTimeout(() => {
+      displayArea(lastThree);
+    }, 100);
+  } else {
+    clearAreaDisplay();
+  }
+}
+
+function fitAreaDisplayText() {
+  const areaDisplay = document.getElementById('areaDisplay');
+  let fontSize = 200;
+  areaDisplay.style.fontSize = fontSize + 'px';
+  const bodyWidth = document.body.clientWidth;
+  while (areaDisplay.scrollWidth > bodyWidth && fontSize > 0) {
+    fontSize--;
+    areaDisplay.style.fontSize = fontSize + 'px';
+  }
+  if (fontSize > 0) {
+    fontSize -= 5;
+    areaDisplay.style.fontSize = fontSize + 'px';
+  }
+}
+
+function displayArea(postalCode) {
+  const areaDisplay = document.getElementById('areaDisplay');
+  const area = areas[postalCode] || "INVALID";
+  areaDisplay.textContent = area;
+  fitAreaDisplayText();
+  document.getElementById('myInput').select();
+}
+
+function clearAreaDisplay() {
+  const areaDisplay = document.getElementById('areaDisplay');
+  areaDisplay.textContent = "";
+}
+
+function adjustViewport() {
+  const currentViewportHeight = window.visualViewport.height;
+  const body = document.body;
+  if (currentViewportHeight < window.innerHeight) {
+    const inputField = document.getElementById('myInput');
+    const offset = (currentViewportHeight - inputField.offsetHeight) / 2 - inputField.offsetHeight;
+    body.style.transform = 'translateY(' + offset + 'px)';
+  } else {
+    body.style.transform = 'none';
+  }
+}
+
+function refocusInputIfNeeded(e) {
+  const inputField = document.getElementById('myInput');
+  if (!inputField.contains(e.target)) {
+    e.preventDefault();
+    inputField.focus();
+  }
+}
+
+window.addEventListener('pageshow', function() {
+  document.body.style.transform = 'none';
+  setTimeout(() => {
+    adjustViewport();
+    fitAreaDisplayText();
+    const areaDisplay = document.getElementById('areaDisplay');
+    if (areaDisplay.textContent.trim().length > 0) {
+      setTimeout(() => {
+        adjustViewport();
+        fitAreaDisplayText();
+      }, 200);
+    }
+  }, 300);
+});
+
+window.addEventListener('resize', adjustViewport);
+window.visualViewport.addEventListener('resize', adjustViewport);
+window.addEventListener('orientationchange', fitAreaDisplayText);
+window.addEventListener('resize', fitAreaDisplayText);
+document.addEventListener('touchmove', function(event) {
+  event.preventDefault();
+}, { passive: false });
+document.addEventListener('touchstart', refocusInputIfNeeded, { passive: false });
+document.addEventListener('mousedown', refocusInputIfNeeded);
